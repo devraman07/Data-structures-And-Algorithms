21b458eee93dd311147a22b7d0e23aeb1ce7b11c
devraman07 <devraman0007@gmail.com>
2026-07-14 15:13:38 +0530

    patterns practice done

--- DataStructures/patterns/practice.java
+++ DataStructures/patterns/practice.java
@@ -118,6 +118,44 @@ public class practice {
         }
     }
 
+    public static int[][] generatePascalTriangle(int n) {
+     int[][] triangle =  new int[n][];
+       
+     for(int i = 0; i < n; i++) {
+        triangle[i] = new int [i + 1];
+        triangle[i][0] = 1;
+        triangle[i][i] = 1;
+        for(int j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i- 1][j];
+        }
+     }
+     
+     return triangle;
+    }
+
+    public static void Butterfly(int n) {
+        for ( int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            } for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            } for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+         for ( int i = n; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            } for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            } for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    } 
+
 
     public static void main(String[] args) {
         // HollowRectangle();
@@ -125,6 +163,15 @@ public class practice {
         // ReverseSolidPyramid();
         // HollowPyramid();
         // NumberTriangle();
-        HollowDiamond();
+        // HollowDiamond();
+        //  int[][] result = generatePascalTriangle(5);
+
+        // for (int[] row : result) {
+        //     for (int val : row) {
+        //         System.out.print(val + " ");
+        //     }
+        //     System.out.println();
+        // }
+        Butterfly(4);
     }
 }
